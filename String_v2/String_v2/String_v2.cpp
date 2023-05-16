21c00a05fc2373a95008fc989d6c5acd7f526921
Arseniy <114307225+arsbars24@users.noreply.github.com>
2023-05-16 11:29:58 +0500

    Add files via upload

---
+++ String_v2/String_v2/String_v2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main() {
+
+    int n, m;
+    string s, s1, s2;
+    cout << "Enter the 1-st string:\n";
+    getline(cin, s1);
+    cout << "Enter the 2-nd string:\n";
+    getline(cin, s2);
+    m = s1.size();
+    n = s2.size(); // string input
+    int** a = new int* [m + 1];
+    for (int i = 0; i < m + 1; i++) {
+        a[i] = new int[n + 1];
+    }
+    for (int j = 0; j <= n; j++) {
+        a[0][j] = 0;
+    }
+    for (int i = 0; i <= m; i++) {
+        a[i][0] = 0;
+    }
+    for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= m; i++) {
+            if (s1[i - 1] == s2[j - 1])
+                a[i][j] = a[i - 1][j - 1] + 1;
+            else
+                a[i][j] = 0;
+        }
+    }// matrix of values
+   
+    for (int j = 0; j <= n; j++) {
+        for (int i = 0; i <= m; i++) {
+            cout.width(2);
+            cout << a[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    s = "";
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (a[i][j] > s.size())
+                s = s1.substr((i - a[i][j] + 1) - 1, a[i][j]);
+        }
+    }
+    cout << "\nTarget substring: " << s << "\n\n";
+    for (int i = 0; i < m + 1; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+    system("pause");
+    return 0;
+}
---
+++ summation/summation/summation.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string> 
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+	string s1, s2;
+	char c1, c2;
+	string in = "Input.txt";
+	//string out = "Output.txt";
+	fstream fs;
+	fs.open(in, fstream::in | fstream::out | fstream::app); //fstream::out;
+	if (!fs.is_open()) {
+		cout << "It`s not!" << endl;
+	}
+	else {
+		cout << "It`s open" << endl;
+	}
+	getline(fs, s1);
+	getline(fs, s2);
+	cout << s1 << endl << s2;
+	for (int i = s1.length(); i > 0; i--) {
+		int k, l, ost;
+		ost = 0;
+		c1 = s1[i];
+		for (int j = s2.length(); j > 0; j--) {
+			c2 = s2[i];
+			k = atoi(&c1);
+			l = atoi(&c2);
+			if (k + l + ost < 10) {
+				s1[i] = itoa(k + l + ost, string, 10);
+			}
+			else {
+				s1[i] = (k + l) % 10;
+				ost = 1;
+			}
+		}
+	}
+	fs << "\n" << s1;
+	fs.close();
+	cout << s1;
+}

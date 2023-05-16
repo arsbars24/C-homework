fdb183025245db916759231b768a20ec4cdfdf9c
Arseniy <114307225+arsbars24@users.noreply.github.com>
2023-05-16 11:41:59 +0500

    Add files via upload

---
+++ Шифр Бекона/Шифр Бекона/Шифр Бекона.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <set>
+
+using namespace std;
+
+int main() {
+    string s;
+
+    cin >> s;
+
+    set<string> SET;      
+
+    for (int i = 0; i < s.length(); i++) {
+        for (int j = 0; j <= s.length() - i; j++) {
+            SET.insert(s.substr(i, j));
+            cout << s.substr(i, j) << " ";
+        }
+    }
+
+    cout << SET.size() << endl;
+    return 0;
+}
---
+++ различные подстроки/различные подстроки/различные подстроки.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int f_c(string S) {
+	int n = S.length();
+	int res = 0;
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n - i; j++){
+			bool bflag = true;
+			for (int k = j + 1; k < n - i; k++) {
+				if (S.substr(j, i) == S.substr(k, i)) {
+					bflag = false;
+					break;
+				}
+
+			}
+			if (bflag) res++;
+		}
+	}
+	return res;
+}

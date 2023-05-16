bb75c4f23d7f0e669c8dad4f1e802b1bc437ac55
Arseniy <114307225+arsbars24@users.noreply.github.com>
2023-05-16 11:32:58 +0500

    Add files via upload

---
+++ tinkoff 3 prog/tinkoff 3 prog/tinkoff 3 prog.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(long int* a, long int* b) {
+    long long int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+long int gcd(long int a,long int b) {
+    if (a < b) {
+        swap(&a, &b);
+    }
+    while (a % b != 0) {
+        a = a % b;
+        swap(&a, &b);
+    }
+    return b;
+}
+
+int main() {
+	long long int n, k;
+    long long int c = 0;
+    long long int C = 0;
+    long long int a = 0;
+	cin >> n;
+    for (int i = 1; i < (n / 2) + 1; i++) {
+        k = n - i;
+        c = gcd(k, n);
+        if (c == 1)
+            continue;
+        else if (c > C)
+            C = c;
+        a = k;
+    }
+    cout << a << ' ' << n - a;
+
+}
---
+++ tinkoff prog1/tinkoff prog1/tinkoff prog1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	char* s = new char[n];
+	char* b = new char[n];
+	for (int i = 0; i < n; i++) {
+		if (b[0] == 'Y') {
+			for (int j = 0; j < n; j++) {
+				
+			}
+			if(s[i] == ' ')
+
+		}
+		else {
+
+		}
+	}
+}
